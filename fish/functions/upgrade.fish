f2503aafb11615af2902e6c86efe2b944ebfb3e5
Az4reus <az4reus@gmail.com>
2017-11-22 06:51:25 +0100

    Includes dotfiles git pull on upgrade function.

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -1,6 +1,12 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.y3qFxE/upgrade.fish @ line 2
+# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.x6821M/upgrade.fish @ line 2
 function upgrade
 	brew upgrade
-  echo "updating vim..."
+
+  echo "Updating Vim..."
   vim +PluginUpdate +qa
+
+  echo "Updating dotfiles..."
+  pushd ~/dotfiles/
+  git pull
+  popd
 end
