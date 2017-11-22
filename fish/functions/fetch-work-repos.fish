4f4f1ab3d3d7f4bf639856e0353f544c2c3987cb
Az4reus <az4reus@gmail.com>
2017-11-22 07:03:10 +0100

    Adds color output to updating repos.

--- fish/functions/fetch-work-repos.fish
+++ fish/functions/fetch-work-repos.fish
@@ -1,8 +1,8 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.K7RVhc/fetch-work-repos.fish @ line 2
+# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.bao275/fetch-work-repos.fish @ line 2
 function fetch-work-repos
 	for dir in ~/work/*
     if test -d $dir 
-      echo "updating $dir"
+      echo (set_color green) "updating $dir" (set_color normal)
       pushd $dir
       git fetch -a --prune
       popd
