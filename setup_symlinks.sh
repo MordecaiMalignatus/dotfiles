f3f23d3ef90c616e2be021a5b0de076df03fc794
Az4reus <az4reus@gmail.com>
2017-12-04 07:53:50 +0100

    Adds tigrc to dotfiles

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -3,6 +3,7 @@ DIR=dirname "$0"
 # Bare dotfiles and folders
 ln -s $DIR/.vimrc     $HOME/.vimrc
 ln -s $DIR/.emacs.d   $HOME/.emacs.d
+ln -s $DIR/.tigrc     $HOME/.tigrc
 ln -s $DIR/.tmux.conf $HOME/.tmux.conf
 ln -s $DIR/.zshrc     $HOME/.zshrc
 ln -s $DIR/.gitconfig $HOME/.gitconfig
