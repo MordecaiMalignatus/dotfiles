fba8d366d8f61f130223242c8d2fa92737f18d28
Az4reus <az4reus@gmail.com>
2017-12-06 11:06:48 +0100

    Adds brew-cask to upgrade function
    after I discovered this does not get run automatically and then spent an
    hour or two building tex. FML.

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -1,6 +1,7 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.x6821M/upgrade.fish @ line 2
+# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.hbWlq0/upgrade.fish @ line 2
 function upgrade
 	brew upgrade
+  brew cask upgrade
 
   echo "Updating Vim..."
   vim +PluginUpdate +qa
