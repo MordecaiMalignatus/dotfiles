ed79608761511d6a95c71703b339076d4d9e8ef1
Az <az4reus@gmail.com>
2017-12-13 09:00:08 +0100

    Adds work screen script, fixes symlink setup

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,4 +1,8 @@
-DIR=dirname "$0"
+# cd into directory of this very script
+# (otherwise the creating of links won't work!)
+# http://stackoverflow.com/a/246128/4568748
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR" || exit 1
 
 # Bare dotfiles and folders
 ln -s $DIR/.vimrc     $HOME/.vimrc
@@ -10,7 +14,7 @@ ln -s $DIR/.gitconfig $HOME/.gitconfig
 
 ## .config folders.
 mkdir -p ~/.config/
-ln -s $DIR/i3         $HOME/.config/i3
-ln -s $DIR/xrandr     $HOME/.config/xrandr
-ln -s $DIR/fish       $HOME/.config/fish
-ln -s $DIR/ranger     $HOME/.config/ranger
+ln -s $DIR/i3         $HOME/.config/
+ln -s $DIR/xrandr     $HOME/.config/
+ln -s $DIR/fish       $HOME/.config/
+ln -s $DIR/ranger     $HOME/.config/
