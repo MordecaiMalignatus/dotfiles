809d733177f3f7d747ce9891cf8257eb1e710df5
Az <az4reus@gmail.com>
2017-12-14 11:07:53 +0100

    Various arch tweaks and fixes

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,1 @@
-function set_up_keybinds
-  bind \ch beginning-of-line
-  bind \cl end-of-line
-  bind \cb backward-word
-  bind \cf foward-word
-  bind \cg accept-autosuggestion
-end
+set -g EDITOR (which vim)
