3183e0f737be6a6aba0c97f79c79905d10a0514e
Az4reus <az4reus@gmail.com>
2017-12-18 19:57:35 +0100

    Removes brew cask from upgrade function

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -1,13 +1,12 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.hbWlq0/upgrade.fish @ line 2
+# Defined in - @ line 2
 function upgrade
 	brew upgrade
-  brew cask upgrade
 
-  echo "Updating Vim..."
-  vim +PluginUpdate +qa
+    echo "Updating Vim..."
+    vim +PluginUpdate +qa
 
-  echo "Updating dotfiles..."
-  pushd ~/dotfiles/
-  git pull
-  popd
+    echo "Updating dotfiles..."
+    pushd ~/dotfiles/
+    git pull
+    popd
 end
