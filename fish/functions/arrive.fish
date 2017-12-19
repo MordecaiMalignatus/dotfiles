ef6c8637137d0dca0f6471072b6b16502709bc3d
Az <az4reus@gmail.com>
2017-12-19 13:55:30 +0100

    Adds task display to arriving

--- fish/functions/arrive.fish
+++ fish/functions/arrive.fish
@@ -1,7 +1,8 @@
-# Defined in /tmp/fish.z5iHQj/arrive.fish @ line 1
+# Defined in /tmp/fish.KcZ7Rd/arrive.fish @ line 2
 function arrive
 	logrs "Arrived at work"
     ssh-add          # blocks until entered
     sudo pacman -Syu # also blocks until sudo is entered
     fetch-work-repos
+    task next
 end
