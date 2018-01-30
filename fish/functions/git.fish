d5cec5026b6d0aac43194c1af759e4653bcaf835
Az4reus <az4reus@gmail.com>
2018-01-30 12:30:45 +0100

    Removes hub alias

--- fish/functions/git.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.zWn1nE/git.fish @ line 1
-function git --description 'Alias for hub, which wraps git to provide extra functionality with GitHub.'
-	hub $argv
-end
