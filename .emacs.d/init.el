e020f84998e07f50d7bf545452d9dae0e1f74d6a
Az4reus <az4reus@gmail.com>
2018-02-02 20:07:13 +0100

    Re-doing my .emacs.d from scratch.
    Still need to blacklist archive contents so the folder doesn't grow
    hueg.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,15 @@
+;; Az' emacs files, the fifteen millionth.  
+;; package config
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Org-mode stuff.
+(setq org-startup-indented 1)
+(add-hook 'org-mode-hook 'auto-fill-mode)
+(setq fill-column 80)
+
 ;; Visuals
 (load-theme 'solarized-light)
 
@@ -14,16 +26,30 @@
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
+;; Helm 
+;(require 'helm-config)
+
 ;; Magit
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; package config
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
-;; Org-mode stuff.
-(setq org-startup-indented 1)
-(add-hook 'org-mode-hook 'auto-fill-mode)
-(setq fill-column 80)
+;; Custom Things.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (paredit magithub helm magit solarized-theme alchemist))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
