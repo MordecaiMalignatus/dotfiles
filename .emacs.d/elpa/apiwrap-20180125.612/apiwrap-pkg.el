72a9b41abd844fc4b220c8c792a547e37f8f6537
Az4reus <az4reus@gmail.com>
2018-02-02 20:23:51 +0100

    Shoving my whole emacs cache into git because screw you.

---
+++ .emacs.d/elpa/apiwrap-20180125.612/apiwrap-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "apiwrap" "20180125.612" "api-wrapping macros" '((emacs "25")) :commit "dfb500b394cfb8332f40d8b9ba344f106fdb9370" :url "https://github.com/vermiculus/apiwrap.el" :keywords '("tools" "maint" "convenience"))
