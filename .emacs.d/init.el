e8654becc5dae711b450793c71892cd1e82c0d4b
Az4reus <az4reus@gmail.com>
2018-02-09 07:03:51 +0100

    Adds SLIME to emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,7 +77,7 @@
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-selected-packages
    (quote
-    (ace-window enh-ruby-mode helm-robe robe paredit magithub helm magit solarized-theme))))
+    (slime ace-window enh-ruby-mode helm-robe robe paredit magithub helm magit solarized-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
