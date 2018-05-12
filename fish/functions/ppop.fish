d644d26aa1fcdc867618ed945293843516fa97d2
Az4reus <az4reus@gmail.com>
2018-05-12 10:17:56 +0200

    Feat(fish): changed ppop function

--- fish/functions/ppop.fish
+++ fish/functions/ppop.fish
@@ -1,1 +1,4 @@
-/home/az/dotfiles/fish/handwritten-functions/ppop.fish
+# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.LZPAvy/ppop.fish @ line 1
+function ppop
+	pockyt get -n 1 -r oldest -s unread -o browser | pockyt mod -a 1 -i redirect
+end
