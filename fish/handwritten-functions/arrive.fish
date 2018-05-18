20b78627445295933dd60e44056d1cc0e46a79c6
Az <az4reus@gmail.com>
2018-05-18 07:29:12 +0200

    Feat(arrive/leave): Hook in screen scripts.

--- fish/handwritten-functions/arrive.fish
+++ fish/handwritten-functions/arrive.fish
@@ -1,7 +1,8 @@
-# Defined in /tmp/fish.utvWl1/arrive.fish @ line 2
+# Defined in /tmp/fish.6Hse7R/arrive.fish @ line 2
 function arrive
 	logrs "Arrived at work"
   ssh-add          # blocks until entered
+  sh ~/dotfiles/xrandr/work-screens.sh
   upgrade
   fetch-work-repos
   task next
--- fish/handwritten-functions/leave.fish
+++ fish/handwritten-functions/leave.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.gSMBy3/leave.fish @ line 2
+# Defined in /tmp/fish.ENt4vq/leave.fish @ line 2
 function leave
 	sudo -v
-	logrs "leaving work"
+  logrs "leaving work"
   i3lock -et -i "/home/az/wallpapers/lockscreen.png"
   sh ~/dotfiles/xrandr/only-laptop-screen.sh
   sudo systemctl hibernate
