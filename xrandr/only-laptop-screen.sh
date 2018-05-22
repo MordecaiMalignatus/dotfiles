69c8c4e7d11cee7df8895c2fffaacb2c9429b52f
Az <az4reus@gmail.com>
2018-05-22 09:22:53 +0200

    Feat(xrandr): Fixes xrandr bug

--- xrandr/only-laptop-screen.sh
+++ xrandr/only-laptop-screen.sh
@@ -1,7 +1,7 @@
 set -e 
 
-xrandr --output DP2-2 --off \
-       --output DP2-1 --off \
-       --output HDMI1 --off \
-       --output HDMI2 --off \
-       --output eDP1 --primary
+xrandr --output DP2-2 --off 
+xrandr --output DP2-1 --off
+xrandr --output HDMI1 --off
+xrandr --output HDMI2 --off
+xrandr --output eDP1 --primary
