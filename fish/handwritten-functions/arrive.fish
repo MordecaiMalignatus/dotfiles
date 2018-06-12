3111a84a123d5619f39a6878e79774dee105a85f
Az <az4reus@gmail.com>
2018-06-12 14:40:08 +0200

    Feat(arrive): Moves sudo call to be before xrandr

--- fish/handwritten-functions/arrive.fish
+++ fish/handwritten-functions/arrive.fish
@@ -1,7 +1,8 @@
-# Defined in /tmp/fish.6Hse7R/arrive.fish @ line 2
+# Defined in /tmp/fish.soNF5j/arrive.fish @ line 2
 function arrive
 	logrs "Arrived at work"
   ssh-add          # blocks until entered
+  sudo -v
   sh ~/dotfiles/xrandr/work-screens.sh
   upgrade
   fetch-work-repos
