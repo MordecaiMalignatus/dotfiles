a942935b8fc73355a3686cffe9193b54496761a5
Mordecai Maligatus <az4reus@gmail.com>
2018-06-27 12:35:27 +0200

    Feat(emacs): Emacs is back, baby. This time actually used.

---
+++ .emacs.d/init.el
@@ -0,0 +1,42 @@
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(tool-bar-mode -1)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(global-set-key (kbd "C-z") 'list-bookmarks)
+
+(defun az-kill-buffer ()
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+(defun init-file ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(load-theme 'gruvbox t)
+ 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ce0788113995714fd96970417e8e71d5182d02bc40cc7ffef307f5e01e55942f" "ed2b5df51c3e1f99207074f8a80beeb61757ab18970e43d57dec34fe21af2433" "d411730c6ed8440b4a2b92948d997c4b71332acf9bb13b31e9445da16445fe43" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (magithub magit w3 gruvbox-theme notmuch solarized-theme paredit org-plus-contrib markdown-mode))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
