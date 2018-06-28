d5d762ae13c6306a5b883a82c018bcd611356476
Mordecai Maligatus <az4reus@gmail.com>
2018-06-28 07:53:58 +0200

    Fix(fish): Fixes typo in leave function.

--- fish/handwritten-functions/leave.fish
+++ fish/handwritten-functions/leave.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.gSMBy3/leave.fish @ line 2
+# Defined in /tmp/fish.Dpk1xh/leave.fish @ line 2
 function leave
-  sudo -v
-  loggrs "leaving work"
+	sudo -v
+  logrs "leaving work"
   i3lock -et -i "/home/az/wallpapers/lockscreen.png"
   sh ~/dotfiles/xrandr/only-laptop-screen.sh
   sudo systemctl hibernate
