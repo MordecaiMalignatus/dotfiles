076f80890e072aaca8023a2da4e3b1f9177eac06
Mordecai Malignatus <az4reus@gmail.com>
2018-07-15 09:17:10 +0200

    Feat(emacs): Adds deft and config for managing notes.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,12 @@
 (set-face-attribute 'default nil :font "PragmataPro-13")
 (load-theme 'gruvbox t)
 
+;; Deft configuration
+(global-set-key "±" 'deft)
+(setq deft-directory "~/Dropbox/Reference")
+(setq deft-use-filename-as-title t)
+(setq deft-recursive t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,4 +38,10 @@
     ("ce0788113995714fd96970417e8e71d5182d02bc40cc7ffef307f5e01e55942f" "ed2b5df51c3e1f99207074f8a80beeb61757ab18970e43d57dec34fe21af2433" "d411730c6ed8440b4a2b92948d997c4b71332acf9bb13b31e9445da16445fe43" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-selected-packages
    (quote
-    (magithub magit gruvbox-theme notmuch solarized-theme paredit org-plus-contrib markdown-mode))))
+    (deft magithub magit gruvbox-theme notmuch solarized-theme paredit org-plus-contrib markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
