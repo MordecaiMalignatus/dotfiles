149ad6369cf924de05b69ecb95243c61148d93d4
Mordecai Malignatus <az4reus@gmail.com>
2018-08-13 08:41:44 +0200

    Chore(Emacs): Custom variables & formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org"          . "https://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
@@ -71,13 +71,13 @@ DIR: The directory that deft should treat as `deft-directory`"
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("ce0788113995714fd96970417e8e71d5182d02bc40cc7ffef307f5e01e55942f" "ed2b5df51c3e1f99207074f8a80beeb61757ab18970e43d57dec34fe21af2433" "d411730c6ed8440b4a2b92948d997c4b71332acf9bb13b31e9445da16445fe43" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("cd4d1a0656fee24dc062b997f54d6f9b7da8f6dc8053ac858f15820f9a04a679" "ce0788113995714fd96970417e8e71d5182d02bc40cc7ffef307f5e01e55942f" "ed2b5df51c3e1f99207074f8a80beeb61757ab18970e43d57dec34fe21af2433" "d411730c6ed8440b4a2b92948d997c4b71332acf9bb13b31e9445da16445fe43" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(org-agenda-files
    (quote
-    ("~/Uni/distributed-systems/examprep.org" "~/Dropbox/Reference/Org/hobby-projects.org")))
+    ("~/Dropbox/Reference/Career/leaving-university.org" "~/Dropbox/Reference/Identity/restoration.org" "~/Dropbox/Reference/Immigration/immigration-overview.org" "~/Dropbox/Reference/Wedding/wedding.org" "~/Dropbox/Reference/Career/full-time-employment.org" "~/Dropbox/Reference/DS/ds.org" "~/projects/haskell-from-first-principles/allen-haskell-from-first-principles.org" "~/Uni/distributed-systems/examprep.org" "~/Dropbox/Reference/Org/hobby-projects.org")))
  '(package-selected-packages
    (quote
-    (exec-path-from-shell flycheck deft magithub magit gruvbox-theme notmuch solarized-theme paredit org-plus-contrib markdown-mode))))
+    (wrap-region haskell-mode alchemist exec-path-from-shell flycheck deft magithub magit gruvbox-theme notmuch solarized-theme paredit org-plus-contrib markdown-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
