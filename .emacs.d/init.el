0f0c234b8457de04a6a57d2e2cbaac37c629daae
Mordecai Malignatus <az4reus@gmail.com>
2018-09-03 19:35:46 +0200

    Fix(emacs): Change melpa-stable to melpa.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("org"          . "https://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (tool-bar-mode -1)
