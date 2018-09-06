1f429da27095b6762a1fb43ff1746b8ada85d843
Mordecai Maligatus <az4reus@gmail.com>
2018-09-06 08:03:16 +0200

    Feat(fish): `ppop' now opens a NN instance after popping an article.

--- fish/functions/ppop.fish
+++ fish/functions/ppop.fish
@@ -1,4 +1,5 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.LZPAvy/ppop.fish @ line 1
+# Defined in /tmp/fish.m5HTeT/ppop.fish @ line 2
 function ppop
 	pockyt get -n 1 -r oldest -s unread -o browser | pockyt mod -a 1 -i redirect
+	nn
 end
