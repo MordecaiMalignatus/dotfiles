cfb7eb5590a893b23a30424536a6d3e782ac73c0
Mordecai Maligatus <az4reus@gmail.com>
2018-09-07 07:24:33 +0200

    Fix(fish): Removes OSX specific global vars.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
-set -g EDITOR (which nvim)
-set -g PER_INBOX /Users/az/Dropbox/Perceptron/@Inbox
+set -g EDITOR (which vim)
 set -g BROWSER (which firefox)
-set -g NN_INBOX /Users/az/Dropbox/Reference/@Inbox
 
 bind \cf forward-word
