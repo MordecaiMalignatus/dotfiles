93a038dcea6c2f90acf800afb7ec2e1e868315d8
MordecaiMaligatus <s.reindl@ryte.com>
2018-09-18 07:45:01 +0200

    Feat(fish): Now kicks off git fetches async

--- fish/handwritten-functions/fetch-work-repos.fish
+++ fish/handwritten-functions/fetch-work-repos.fish
@@ -1,10 +1,10 @@
-# Defined in /var/folders/g7/r4g4b9_n1rqd3gk8fdq9d6wm0000gn/T//fish.bao275/fetch-work-repos.fish @ line 2
+# Defined in /tmp/fish.SzY84l/fetch-work-repos.fish @ line 2
 function fetch-work-repos
 	for dir in ~/work/*
     if test -d $dir 
       echo (set_color green) "updating $dir" (set_color normal)
       pushd $dir
-      git fetch -a --prune
+      git fetch -a --prune &
       popd
     else
       echo "skipping $dir"
