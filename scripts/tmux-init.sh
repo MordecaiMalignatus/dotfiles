91c8e24b14cfbe293b6a9c5a566d97ef186ec0b7
MordecaiMaligatus <s.reindl@ryte.com>
2018-10-09 07:19:22 +0200

    Feat(startup): Correctly start `arrive` on tmux-init.sh

--- scripts/tmux-init.sh
+++ scripts/tmux-init.sh
@@ -8,7 +8,7 @@ MAIN_WINDOW_NAME="command-central"
 
 if ! (tmux has-session -t $SESSION_NAME 2>/dev/null); then
   tmux new -d -s $SESSION_NAME -c "$HOME"
-  tmux new-window -t $SESSION_NAME:2 -n $MAIN_WINDOW_NAME -c $HOME arrive
+  tmux new-window -t $SESSION_NAME:2 -n $MAIN_WINDOW_NAME -c $HOME 'fish -iC "arrive"'
   tmux split-window -t $SESSION_NAME:2 -h
   tmux new-window -t $SESSION_NAME:3 -n localdev -c "$HOME/work/docker-localdev"
   tmux kill-window -t $SESSION_NAME:1
