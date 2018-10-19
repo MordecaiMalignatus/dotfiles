e83aeb2205c05a2cb0a29e8e35e95e6f3e4b11be
Steffen Reindl <s.reindl@ryte.com>
2018-10-19 07:37:35 +0200

    Feat(i3): Arrange windows when arriving at work.

--- fish/handwritten-functions/arrive.fish
+++ fish/handwritten-functions/arrive.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.10V90o/arrive.fish @ line 2
+# Defined in /tmp/fish.efFVbT/arrive.fish @ line 2
 function arrive
 	logrs "Arrived at work"
-  sudo -v
   sh ~/dotfiles/xrandr/work-screens.sh
+  sh ~/dotfiles/scripts/i3-arrangement.sh
   upgrade
   fetch-work-repos
   emacs &
---
+++ scripts/i3-arrangement.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+echo "Arranging i3..."
+
+i3 workspace Nerdery\; move workspace to output DP2-1
+i3 workspace Emacs\; move workspace to output DP2-1
+i3 workspace Browser\; move workspace to output DP2-2
