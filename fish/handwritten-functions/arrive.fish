eba3d9fd41f2f75ce8f2c9ec54e4d71d286d23e6
Steffen Reindl <s.reindl@ryte.com>
2018-10-31 09:21:23 +0100

    Fix(startup): Start emacs before reordering i3 screens.

--- fish/handwritten-functions/arrive.fish
+++ fish/handwritten-functions/arrive.fish
@@ -1,11 +1,11 @@
-# Defined in /tmp/fish.efFVbT/arrive.fish @ line 2
+# Defined in /tmp/fish.6xwAGx/arrive.fish @ line 2
 function arrive
 	logrs "Arrived at work"
+  emacs &
   sh ~/dotfiles/xrandr/work-screens.sh
   sh ~/dotfiles/scripts/i3-arrangement.sh
   upgrade
   fetch-work-repos
-  emacs &
   worktimecalc.py
   inbox-status
 end
