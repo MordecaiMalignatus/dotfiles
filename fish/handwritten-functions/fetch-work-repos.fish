4abe82d6472a544ff603acb029ad6339d652fbd9
Steffen Reindl <s.reindl@ryte.com>
2018-11-07 08:57:18 +0100

    Feat(scripts): Nulls output of fetch work repos.

--- fish/handwritten-functions/fetch-work-repos.fish
+++ fish/handwritten-functions/fetch-work-repos.fish
@@ -1,10 +1,10 @@
-# Defined in /tmp/fish.0Ra4l7/fetch-work-repos.fish @ line 2
+# Defined in /tmp/fish.Pf6slL/fetch-work-repos.fish @ line 2
 function fetch-work-repos
 	for dir in ~/work/*
     if test -d $dir 
       echo (set_color green) "updating $dir" (set_color normal)
       pushd $dir
-      git fetch -a --prune > /dev/null &
+      git fetch -a --prune 2> /dev/null &
       popd
     else
       echo "skipping $dir"
