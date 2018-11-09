88dcb8eb5b671efbbd8795432017071317881b75
Steffen Reindl <s.reindl@ryte.com>
2018-11-09 15:15:52 +0100

    Feat(distractable.sh): Add fefe to block list.

--- scripts/distractable.sh
+++ scripts/distractable.sh
@@ -6,6 +6,7 @@ echo "Blocking distractions..."
 sudo -v
 
 sudo sed -i "$ a 0.0.0.0 hckrnews.com" /etc/hosts
+sudo sed -i "$ a 0.0.0.0 blog.fefe.de" /etc/hosts
 sudo sed -i "$ a 0.0.0.0 news.ycombinator.com" /etc/hosts
 sudo sed -i "$ a 0.0.0.0 www.reddit.com" /etc/hosts
 sudo sed -i "$ a 0.0.0.0 www.nytimes.com" /etc/hosts
