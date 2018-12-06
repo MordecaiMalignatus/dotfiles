1f3925493752f38679b83df6dcef56f100a82ffa
Steffen Reindl <s.reindl@ryte.com>
2018-12-06 11:18:00 +0100

    Feat(tmux.sh): Edit starting windows for tmux

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -13,7 +13,9 @@ if ! (tmux has-session -t $SESSION_NAME 2>/dev/null); then
   tmux new -d -s $SESSION_NAME -c "$HOME"
   tmux new-window -t $SESSION_NAME:2 -n $MAIN_WINDOW_NAME -c $HOME 'fish -iC "arrive"'
   tmux split-window -t $SESSION_NAME:2 -h
-  tmux new-window -t $SESSION_NAME:3 -n localdev -c "$HOME/work/docker-localdev"
+  tmux new-window -t $SESSION_NAME:3 -n wat-infrastructure -c "$HOME/work/wat-infrastructure"
+  tmux new-window -t $SESSION_NAME:4 -n crawler-stream -c "$HOME/work/crawler-stream"
+  tmux new-window -t $SESSION_NAME:5 -n zoom-importer -c "$HOME/work/zoom-importer"
   tmux kill-window -t $SESSION_NAME:1
 
   tmux attach -t $SESSION_NAME:1.0
