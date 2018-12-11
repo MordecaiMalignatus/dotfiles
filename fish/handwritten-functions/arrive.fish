fe2c1abc529adfc1874e7fc331cca4719cb10036
Steffen Reindl <s.reindl@ryte.com>
2018-12-11 08:13:18 +0100

    Feat(arrive): Focus console after arranging, open auth sites.

--- fish/handwritten-functions/arrive.fish
+++ fish/handwritten-functions/arrive.fish
@@ -1,10 +1,12 @@
 # Defined in /tmp/fish.nB77mJ/arrive.fish @ line 2
 function arrive
-	logrs "Arrived at work"
+  logrs "Arrived at work"
   emacs &
   sh ~/dotfiles/xrandr/work-screens.sh
   sh ~/dotfiles/scripts/i3-arrangement.sh
+  i3-msg workspace Nerdery
   upgrade
   fetch-work-repos
   worktimecalc.py
+  sh ~/dotfiles/scripts/open-auth-sites.sh
 end
