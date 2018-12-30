a2e131c2577e449a60b70664eea68bf5b2c04f10
Mordecai Malignatus <az4reus@gmail.com>
2018-12-30 13:09:54 +0100

    Feat(emacs): Add auto-updating packages.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,6 +36,14 @@
 
 (add-hook 'after-init-hook 'load-init-settings)
 
+;; Auto-upgrade packages and delete old ones.
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t
+         auto-package-update-interval 4)
+   (auto-package-update-maybe))
+
 ;; Replace default help functions with `helpful'
 (use-package helpful
   :ensure t
