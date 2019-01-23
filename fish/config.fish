de445fe9637f0fb40f165455db76b98738d85d7b
Steffen Reindl <s.reindl@ryte.com>
2019-01-23 07:30:40 +0100

    Feat(fish): Move abbrs to declarative file
    Also remove erroneous config file

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,12 @@ set -g EDITOR (which vim)
 set -g BROWSER (which firefox)
 
 bind \cf forward-word
+
+if not set -q abbr_init
+   set -U abbr_init t
+   echo -n "Setting abbrevs"
+
+   abbr tsh 'tmux.sh'
+
+   echo -n "Set fish abbreviations..."
+end
