40e08badaa28e245345ebc91ef2936b4502e3970
Steffen Reindl <s.reindl@ryte.com>
2019-02-04 10:53:09 +0100

    feat(fish): Remove ~nn~ call.

--- fish/handwritten-functions/ppop.fish
+++ fish/handwritten-functions/ppop.fish
@@ -1,5 +1,4 @@
-# Defined in /tmp/fish.m5HTeT/ppop.fish @ line 2
+# Defined in /tmp/fish.aqBvgC/ppop.fish @ line 2
 function ppop
 	pockyt get -n 1 -r oldest -s unread -o browser | pockyt mod -a 1 -i redirect
-	nn
 end
