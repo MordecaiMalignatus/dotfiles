1e7c9ae3af574684764bcad78806f43f8b29497c
Steffen Reindl <s.reindl@ryte.com>
2019-03-12 07:31:41 +0100

    Fix(arrive): Remove Auth sites call and script.

--- fish/handwritten-functions/arrive.fish
+++ fish/handwritten-functions/arrive.fish
@@ -1,10 +1,9 @@
 # Defined in /tmp/fish.nB77mJ/arrive.fish @ line 2
 function arrive
   logrs "Arrived at work"
-  emacs &
+  emacs &; disown
   sh ~/dotfiles/xrandr/work-screens.sh
   sh ~/dotfiles/scripts/i3-arrangement.sh
-  sh ~/dotfiles/scripts/open-auth-sites.sh
   i3-msg workspace Nerdery
   fetch-work-repos
   worktimecalc.py
