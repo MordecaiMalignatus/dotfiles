b33e7249d050775690e74f29eb6bcc2b98bdb609
Steffen Reindl <s.reindl@ryte.com>
2019-03-22 08:41:54 +0100

    Fix(i3-arrangement.sh): Adapt target screens to one being vertical

--- scripts/i3-arrangement.sh
+++ scripts/i3-arrangement.sh
@@ -2,7 +2,7 @@
 
 echo "Arranging i3..."
 
-i3-msg workspace IntelliJ\; move workspace to output DP2-2
+i3-msg workspace IntelliJ\; move workspace to output DP2-1
 i3-msg workspace Nerdery\; move workspace to output DP2-1
-i3-msg workspace Emacs\; move workspace to output DP2-1
+i3-msg workspace Emacs\; move workspace to output DP2-2
 i3-msg workspace Browser\; move workspace to output DP2-2
