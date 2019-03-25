dcf5bdaa7ef9519eb079514b6ae1cdc0393e5c29
Steffen Reindl <s.reindl@ryte.com>
2019-03-25 10:21:11 +0100

    Feat(xrandr): Turn off the laptop screen while docked.

--- xrandr/work-screens.sh
+++ xrandr/work-screens.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 4000x360 --rotate normal
+xrandr --output eDP1 --off
 xrandr --output DP1 --off
-xrandr --output DP2-1 --mode 2560x1440 --pos 1440x0 --rotate normal
+xrandr --output DP2-1 --mode 2560x1440 --pos 1440x544 --rotate normal
 xrandr --output DP2-2 --mode 2560x1440 --pos 0x0 --rotate right
 xrandr --output DP2-3 --off
 xrandr --output HDMI2 --off
