bef83843941af4464e299d87f9e81ef3b951b5b4
Steffen Reindl <s.reindl@ryte.com>
2019-03-28 09:37:18 +0100

    Fix(xrandr): Correct screen placement/primary screen.

--- xrandr/work-screens.sh
+++ xrandr/work-screens.sh
@@ -2,8 +2,8 @@
 xrandr --output VIRTUAL1 --off
 xrandr --output eDP1 --off
 xrandr --output DP1 --off
-xrandr --output DP2-1 --mode 2560x1440 --pos 1440x544 --rotate normal
 xrandr --output DP2-2 --mode 2560x1440 --pos 0x0 --rotate right
+xrandr --output DP2-1 --primary --mode 2560x1440 --pos 1440x544 --rotate normal
 xrandr --output DP2-3 --off
 xrandr --output HDMI2 --off
 xrandr --output HDMI1 --off
