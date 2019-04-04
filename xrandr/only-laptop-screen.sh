1b3434dc941d6727668868a25fadcb220e15d78e
Steffen Reindl <s.reindl@ryte.com>
2019-04-04 16:05:04 +0200

    Fix(xrandr): Fix up laptop-only.sh

--- xrandr/only-laptop-screen.sh
+++ xrandr/only-laptop-screen.sh
@@ -1,7 +1,2 @@
-set -e 
-
-xrandr --output DP2-2 --off 
-xrandr --output DP2-1 --off
-xrandr --output HDMI1 --off
-xrandr --output HDMI2 --off
-xrandr --output eDP1 --primary
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
