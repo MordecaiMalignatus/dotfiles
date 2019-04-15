9d7611fba1e14b03597a52a2367826dea7ec6bda
Steffen Reindl <s.reindl@ryte.com>
2019-04-15 15:34:10 +0200

    Refactor(org-kasten.el): Change the reference home to be a funtion.

--- .emacs.d/init/org-kasten.el
+++ .emacs.d/init/org-kasten.el
@@ -13,15 +13,13 @@
   "Your home for the kasten.
 If nil, org-kasten won't do anything.")
 
-;; TODO: This needs to be removed and just replaced with a static
-;; concatenation. There is no need to ever set this separately.
-(defvar org-kasten-references-home (if (eq nil org-kasten-home)
-					 nil
-				       (concat org-kasten-home "References/"))
-  "Home for your bibliographic references and notes.
-You can set this to be the same as your kasten, but I recommend
-keeping it separate.  Files in this will be prefixed with 'R' to
-keep them in a separate index.")
+(defun org-kasten--reference-dir ()
+  "Home to your references and your otherwise notes.
+Located in `org-kasten-home'/References."
+  (if (eq nil org-kasten-home)
+      nil
+    (concat org-kasten-home "References/")))
+
 
 ;; TODO: I need to merge references and links if I'm going to distinguish the
 ;; two in form of links regardless. Then one header field would fall away and it
