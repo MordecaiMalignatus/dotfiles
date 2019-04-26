15094654829698f82d07f4fc4b7475424b669e8b
Steffen Reindl <s.reindl@ryte.com>
2019-04-26 07:41:01 +0200

    Feat(emacs): Add YASnippets, ticket template.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -175,6 +175,8 @@
 (use-package yasnippet
   :ensure t
   :config
+  (setq yas-snippet-dirs
+	(list (concat user-emacs-directory "snippets")))
   (yas-global-mode 1))
 
 (use-package yasnippet-snippets
