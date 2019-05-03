8187cfbdbed1bea89dd60966eb4077b2621871fd
Steffen Reindl <s.reindl@ryte.com>
2019-05-03 13:52:04 +0200

    Fix(xrandr): Fix copying error.

--- xrandr/work-screens.sh
+++ xrandr/work-screens.sh
@@ -3,7 +3,7 @@ xrandr --output VIRTUAL1 --off
 xrandr --output eDP1 --off
 xrandr --output DP1 --off
 xrandr --output DP2-2 --mode 2560x1440 --pos 0x0 --rotate right
-xrandr --output DP1 --primary ----mode 4096x2304 --pos 1440x128 --rotate normal
+xrandr --output DP1 --primary --mode 4096x2304 --pos 1440x128 --rotate normal
 xrandr --output DP2-3 --off
 xrandr --output HDMI2 --off
 xrandr --output HDMI1 --off
