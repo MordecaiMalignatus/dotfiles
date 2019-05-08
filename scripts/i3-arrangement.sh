645a9470fcd1adfdb7c6460f6cfc902f6eda30ed
Steffen Reindl <s.reindl@ryte.com>
2019-05-08 15:11:03 +0200

    Feat(i3-arrangement.sh): Move emacs to horizontal screen.

--- scripts/i3-arrangement.sh
+++ scripts/i3-arrangement.sh
@@ -4,5 +4,5 @@ echo "Arranging i3..."
 
 i3-msg workspace IntelliJ\; move workspace to output DP1
 i3-msg workspace Nerdery\; move workspace to output DP1
-i3-msg workspace Emacs\; move workspace to output DP2-2
+i3-msg workspace Emacs\; move workspace to output DP1
 i3-msg workspace Browser\; move workspace to output DP2-2
