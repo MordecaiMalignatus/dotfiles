8d6b1111e85e2a9b1a1155f1ebe468463531e47d
Steffen Reindl <s.reindl@ryte.com>
2019-05-31 10:24:56 +0200

    Fix(fish): Remove unneeded workarounds from leave function

--- fish/handwritten-functions/leave.fish
+++ fish/handwritten-functions/leave.fish
@@ -1,8 +1,6 @@
-# Defined in /tmp/fish.yYEidO/leave.fish @ line 2
+# Defined in /tmp/fish.zIUHW0/leave.fish @ line 2
 function leave
 	sudo -v
   logrs "leaving work"
-  i3lock -et -i "/home/az/wallpapers/lockscreen.png"
-  sh ~/dotfiles/xrandr/only-laptop-screen.sh
   sudo shutdown now
 end
