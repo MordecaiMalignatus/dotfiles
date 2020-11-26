50e62c226d920ce3b0d7997ff82524b8447a4690
Steffen Reindl <steffen.reindl@pricehubble.com>
2020-11-26 08:33:53 +0100

    pyenv-venv init, gcloud, misc aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,18 @@ set -g BROWSER (which firefox)
 bind \cf forward-word
 
 status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
 eval (direnv hook fish)
 
-# Call sq during shell init. 
-if isatty 
+# Aliases
+alias k=kubectl
+alias ist=istioctl
+
+# Call sq during shell init.
+if isatty
   sq status
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/az/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/az/Downloads/google-cloud-sdk/path.fish.inc'; end
