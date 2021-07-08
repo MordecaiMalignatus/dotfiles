aaf8f2136470a5976e37c67b7a256af7618c8fbf
MordecaiMalignatus <mordecai@malignat.us>
2021-07-08 15:08:47 +0200

    Fix(fish): Correct spelling error

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -1,10 +1,14 @@
 function upgrade
-    echo (set_color green)"Upgrading Packages"(set_color normal)
+    echo (set_color green)"Upgrading Packages..."(set_color normal)
     brew upgrade
+
     if test hostname = "ALT01896\n"
-      echo (set_color green)"Upgrading Vault"(set_color normal)
+      echo (set_color green)"Upgrading Vault..."(set_color normal)
       vault wf upgrade
     end
+
+    echo (set_color green)"Updating gcloud CLI..."(set_color normal)
+    gcloud components update
 
     echo (set_color green)"Updating dotfiles..."(set_color normal)
     pushd ~/dotfiles/
