d92c6c4853648f19902c0d6d7a39868703a554a1
MordecaiMalignatus <mordecai@malignat.us>
2021-07-09 15:52:50 +0200

    Feat(hammerspoon): Spawn terminal on each invocation

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,19 +1,14 @@
 -- Reload config any time. 
 hs.hotkey.bind({"shift", "ctrl"}, "R", function()
     hs.reload()
-    hs.notify.new({title="Config reloaded"}):send()
 end)
 
 -- Terminal automation
 hs.hotkey.bind({"shift", "ctrl"}, "D", function()
     local spawnNewTerminal = [[
         tell application "Terminal"
-                if (exists window 1) then
-                        activate
-                else
-                        do script ""
-                        activate
-                end
+          do script ""
+          activate
         end tell
     ]]
     local success, object, description = hs.osascript.applescript(spawnNewTerminal)
