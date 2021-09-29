aff3c5ceabd8326e32c9541bcda8aab5cb237ede
MordecaiMalignatus <mordecai@malignat.us>
2021-09-29 09:20:29 +0200

    Feat(hammerspoon): Add confirmation sound on execution of hotkeys.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,7 @@
 -- Reload config any time. 
 hs.hotkey.bind({"shift", "ctrl"}, "R", function()
     hs.reload()
+    hs.sound.getByName("Ping"):play()
 end)
 
 -- Terminal automation
@@ -16,4 +17,9 @@ hs.hotkey.bind({"shift", "ctrl"}, "D", function()
         hs.notify.new({title="Spawning new Terminal not successful :(",
             informativeText="Error message: " .. description}):send()
     end
+end)
+
+hs.hotkey.bind({"ctrl", "alt", "shift"}, "W", function() 
+    hs.execute("pass -c wf", true)
+    hs.sound.getByName("Ping"):play()
 end)
