68522094af4df3cc7ee5ca65d0d9ecdec4aa8e41
Mordecai Malignatus <mordecai@malignat.us>
2021-10-10 13:56:49 +0200

    Fix(fish): Only upgrade gcloud on work machine

--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,7 @@ fish_add_path ~/go/bin/
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/elixir-ls
 fish_add_path ~/projects/wf/scripts
+fish_add_path /usr/local/opt/curl/bin
 
 if test (hostname) = "albatross"
         sq
