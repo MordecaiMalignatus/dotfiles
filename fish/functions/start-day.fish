825c12baeb09c4dddbcf24c99ddae0a499086a62
Mordecai Malignatus <mordecai@malignat.us>
2021-12-31 11:03:32 +0100

    Feat(fish): Make start-day checklist host universal.

--- fish/functions/start-day.fish
+++ fish/functions/start-day.fish
@@ -1,3 +1,8 @@
 function start-day
-        cls -f ~/projects/shed/checklists/start.md
+        switch (hostname)
+                case "albatross"
+                        cls -f ~/Sync/Checklists/morning.md
+                case "ALT00622"
+                        cls -f ~/projects/shed/checklists/start.md
+        end
 end
