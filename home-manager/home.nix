79c2dcd1a1e2d22c105d954afa5199e4c048ef9a
Steffen Reindl <steffen.reindl@terramate.io>
2023-07-25 14:03:30 +0200

    Feat(home-manager): Create config, remove names and stuff form it.

---
+++ home-manager/home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+let
+  userdata = import ./userdata.nix;
+in
+{
+  # nodoxxpls.
+  home.username = userdata.username;
+  home.homeDirectory = userdata.homeDirectory;
+
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  home.packages = [
+    pkgs.visidata
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/steffenreindl/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
