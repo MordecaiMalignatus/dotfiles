ae8453c6bbd78d206e210cacebf421f99f768a51
Steffen Reindl <steffen.reindl@terramate.io>
2023-07-25 14:43:04 +0200

    Feat(home-manager): Start moving dotfile linking to home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,6 +2,7 @@
 
 let
   userdata = import ./userdata.nix;
+  dotfiles = "${userdata.homeDirectory}/dotfiles";
 in
 {
   # nodoxxpls.
@@ -12,40 +13,20 @@ in
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
+  programs.home-manager.enable = true;
 
   home.packages = [
     pkgs.visidata
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".vimrc".source     = "${dotfiles}/.vimrc";
+    ".tmux.conf".source = "${dotfiles}/.tmux.conf";
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/steffenreindl/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+
 }
