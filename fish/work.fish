c362c493fc6e74d28be33060bd7e6683cc639354
Steffen Reindl <sar@stripe.com>
2024-03-05 10:37:29 +0100

    Feat(fish): Renovate work config layout and setup.

--- fish/work.fish
+++ fish/work.fish
@@ -1,5 +1,13 @@
-# Make "go install" go via the appropriate proxy.
-set -gx GOPROXY 'https://:@artifactorybase.service.csnzoo.com/artifactory/api/go/go/'
-set -gx GOPRIVATE 'github.csnzoo.com'
+# Work-specific config. Currently, this is for Stripe.
+# As recommended in go/fish-shell
+source (rbenv init -|psub)
+source ~/stripe/space-commander/bin/sc-env-activate.fish
+functions -e fish_right_prompt
 
-fish_add_path ~/projects/shed/scripts
+## Work sources
+fish_add_path "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+fish_add_path "$HOME/.rbenv/shims"
+fish_add_path "$HOME/.rbenv/bin"
+fish_add_path "$HOME/stripe/password-vault/bin"
+fish_add_path "$HOME/stripe/space-commander/bin"
+fish_add_path "$HOME/stripe/henson/bin"
