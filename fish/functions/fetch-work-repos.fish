4189bd34efaba55aaddee7034b25b2dcf4bd14b1
Steffen Reindl <sar@stripe.com>
2024-07-08 10:25:17 +0200

    fish: linearize `fetch-work-repos`.

--- fish/functions/fetch-work-repos.fish
+++ fish/functions/fetch-work-repos.fish
@@ -1,10 +1,10 @@
 # Defined in /tmp/fish.Pf6slL/fetch-work-repos.fish @ line 2
 function fetch-work-repos
-	for dir in ~/work/*
+  for dir in ~/stripe/*
     if test -d $dir 
       echo (set_color green) "updating $dir" (set_color normal)
       pushd $dir
-      git fetch -a --prune 2> /dev/null &
+      git fetch -a --prune
       popd
     else
       echo "skipping $dir"
