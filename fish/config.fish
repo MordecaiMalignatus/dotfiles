7960a6a1b0b0b26b7b4f7f3fd842e74ad37a10ce
Mordecai Malignatus <mordecai@malignat.us>
2024-08-21 10:50:12 +0200

    feat(fish): Add OPAM sourcing

--- fish/config.fish
+++ fish/config.fish
@@ -38,3 +38,12 @@ fish_add_path ~/go/bin/
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/elixir-ls
 fish_add_path /opt/homebrew/bin
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/az/.opam/opam-init/init.fish' && source '/Users/az/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
