bbbfad40d7449a443950b3a325f218e7b072bb22
Mordecai Malignatus <mordecai@malignat.us>
2025-01-02 08:31:06 +0100

    feat(fish): add bun include directives

--- fish/config.fish
+++ fish/config.fish
@@ -47,3 +47,7 @@ fish_add_path /opt/homebrew/bin
 # This section can be safely removed at any time if needed.
 test -r '/Users/az/.opam/opam-init/init.fish' && source '/Users/az/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
 # END opam configuration
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
