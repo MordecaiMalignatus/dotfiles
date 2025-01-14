acc5702b2482918abc09e0d9ddfb5a00d6fb6a46
Mordecai Malignatus <mordecai@malignat.us>
2025-01-14 10:13:39 +0100

    fish: remove the branch switching on upgrade

--- fish/functions/fetch-work-repos.fish
+++ fish/functions/fetch-work-repos.fish
@@ -1,13 +1,10 @@
 # Defined in /tmp/fish.Pf6slL/fetch-work-repos.fish @ line 2
 function fetch-work-repos
   for dir in ~/src/*
-    if test -d $dir 
+    if test -d $dir
       echo (set_color green) "updating $dir" (set_color normal)
       pushd $dir
       git fetch -a --prune
-      git checkout master
-      git pull
-      git switch -
       popd
     else
       echo "skipping $dir"
