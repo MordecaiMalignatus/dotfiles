b5a7ca3440d03f988fd34c44b3fc788a8a0f0d50
Mordecai Malignatus <mordecai@malignat.us>
2025-01-29 11:24:43 +0100

    fix(fish): remove OPAM/bun config cruft

--- fish/config.fish
+++ fish/config.fish
@@ -41,15 +41,3 @@ end
 if test -d /opt/puppetlabs/bin
         fish_add_path /opt/puppetlabs/bin
 end
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/Users/az/.opam/opam-init/init.fish' && source '/Users/az/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
