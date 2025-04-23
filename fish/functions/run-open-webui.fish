2e905e8b64ea60154eb0704331f450ebedb6cfa0
Mordecai Malignatus <mordecai@malignat.us>
2025-04-23 09:38:40 +0200

    fix(fish/run-open-webui): disable auth

--- fish/functions/run-open-webui.fish
+++ fish/functions/run-open-webui.fish
@@ -1,3 +1,3 @@
 function run-open-webui
-        docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+        docker run -d -p 3000:8080 -e WEBUI_AUTH=False --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 end
