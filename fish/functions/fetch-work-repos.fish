8ce0efd4c9739b4d30e5cc8f4c5de29904d19e24
Mordecai Malignatus <mordecai@malignat.us>
2026-08-17 10:39:36 +0200

    fish/fetch-work-repos: Skip dir if it starts with _

--- fish/functions/fetch-work-repos.fish
+++ fish/functions/fetch-work-repos.fish
@@ -2,6 +2,9 @@
 function fetch-work-repos
   for dir in ~/src/*
     if test -d $dir
+      if string match -qe "_" $dir
+        continue
+      end
       echo (set_color green) "updating $dir" (set_color normal)
       pushd $dir
       git fetch -a --prune
